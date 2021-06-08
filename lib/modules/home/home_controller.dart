50cea812d4c9fe3604dddfc2fb5ffe4a6fb0882f
Isaias MacOS <isaiasgoncalves.igs@gmail.com>
2021-06-08 19:17:18 -0300

    getx em filtroStatusMesa

--- lib/modules/home/home_controller.dart
+++ lib/modules/home/home_controller.dart
@@ -2,4 +2,12 @@ import 'package:get/get.dart';
 
 class HomeController extends GetxController {
   var qtdMesas = 27.obs;
+
+  var _filtroMesa = "ALL".obs;
+  String get filtroMesa => _filtroMesa.value;
+
+  void setFiltroMesa(String novo) {
+    _filtroMesa.value = novo;
+    // TODO: FILTRAR LISTA DE MESAS
+  }
 }
--- lib/modules/home/widgets/menusup_home.dart
+++ lib/modules/home/widgets/menusup_home.dart
@@ -1,11 +1,14 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:wklcomanda/modules/home/home_controller.dart';
 import 'package:wklcomanda/modules/home/widgets/button_atualizar.dart';
-import 'package:wklcomanda/modules/home/widgets/custom_ppmenuitem.dart';
 
 class MenuSuperiorHome extends StatelessWidget {
-  const MenuSuperiorHome({Key? key}) : super(key: key);
+  MenuSuperiorHome({Key? key}) : super(key: key);
+
+  final HomeController _homeController = Get.find();
 
   @override
   Widget build(BuildContext context) {
@@ -30,29 +33,61 @@ class MenuSuperiorHome extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           _menuPopup(),
-          // _menuDropDown(),
           ButtonAtualizar(),
         ],
       ),
     );
   }
 
-  PopupMenuButton<String> _menuPopup() {
+  Widget _menuPopup() {
+    ///
+    PopupMenuItem<String> _item(String value, String text, Color circleColor) {
+      // String value = "";
+      // String text = "";
+      // Color circleColor = Colors.white;
+      const double altura = 40;
+      // const double width = 120;
+
+      return PopupMenuItem(
+        height: altura,
+        value: value,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              radius: 7,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(radius: 5.5, backgroundColor: circleColor),
+            ),
+            SizedBox(width: 5),
+            Text(text, style: TextStyle(color: Colors.white)),
+            Obx(
+              () => (value == _homeController.filtroMesa)
+                  ? Checkbox(value: true, onChanged: null)
+                  : Container(),
+            ),
+          ],
+        ),
+      );
+    }
+
     return PopupMenuButton<String>(
       color: Colors.black,
       offset: Offset.fromDirection(90, 25),
       padding: const EdgeInsets.only(top: 35),
-      onSelected: (val) {},
+      //initialValue: _homeController.filtroMesa,
+      onSelected: (val) {
+        _homeController.setFiltroMesa(val);
+      },
       // icon: Icon(Icons.menu),
       child: _rowStatusMesas(),
 
       itemBuilder: (_) => [
-        // TODO: SET FILTROATUAL POR VARIABLE EM HOME CONTROLLER
-        CustomPpMenuItem.all(filtroAtual: "FREE"),
-
-        CustomPpMenuItem.ocupadas(filtroAtual: "OCUP"),
-        CustomPpMenuItem.livres(filtroAtual: "FREE"),
-        CustomPpMenuItem.dormindo(filtroAtual: "FREE"),
+        _item("ALL", "Todas", Colors.white),
+        _item("OCUP", "Ocupadas", Colors.black),
+        _item("FREE", "Livres", Colors.green),
+        //_item("testin", "TESTE", Colors.blue),
+        _item("ZZZ", "Dormindo", Colors.orange),
       ],
       tooltip: "Clique para filtrar",
     );
